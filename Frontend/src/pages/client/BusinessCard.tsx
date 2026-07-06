4dffeabdc39254c7c0381a24302204fe6e2e3c64
Merin38pr <146703241+Merin38pr@users.noreply.github.com>
2026-07-06 10:37:50 +0530

    BusinessCard component with multiple sections

---
+++ Frontend/src/pages/client/BusinessCard.tsx
@@ -0,0 +1,35 @@
+import HeroSection from "../../components/client/HeroSection";
+import MainContent from "../../components/client/MainContent";
+import CompanySection from "../../components/client/CompanySection";
+import KnowMoreSection from "../../components/client/KnowMoreSection";
+import TrustedSection from "../../components/client/TrustedSection";
+import Footer from "../../components/client/Footer";
+
+import { employee } from "../../data/client/employee";
+import { company } from "../../data/company/company";
+
+function BusinessCard() {
+  return (
+    <main className="bg-[#FAF9FF] text-slate-900">
+
+      <HeroSection
+        employee={employee}
+        company={company}
+      />
+
+      <MainContent employee={employee} />
+
+      <CompanySection company={company} />
+
+      <KnowMoreSection />
+
+      <TrustedSection />
+
+      <Footer />
+
+    </main>
+  );
+}
+
+export default BusinessCard;
+
